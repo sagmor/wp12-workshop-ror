88cde0764a2b5f921e01d6f884d00cdb844458a4
Seba Gamboa <me@sagmor.com>
2012-11-16 12:52:49 -0300

    Arreglar Callback de facebook

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ TodoApp::Application.routes.draw do
   get :signin, to: 'sessions#new'
   get :signout, to: 'sessions#destroy'
   
+  get '/auth/facebook/callback', to: 'sessions#create'
+  
   resources :users
 end
